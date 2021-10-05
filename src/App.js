e551e0dc6f2eaaa5adae3744ee6e2551ff8376ba
Rubin Barclay <rubinbarclay@protonmail.com>
2021-10-05 13:38:50 +0200

    Sidebar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar/Sidebar";
+import Feed from "./components/Feed/Feed";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Feed />
     </div>
   );
 }
